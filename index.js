6c6605733940b0c8b41875cd63b90d5b870b210a
ObjectObjectObjectObjectObject <83755436+ObjectObjectObjectObjectObject@users.noreply.github.com>
2021-05-06 18:26:02 +0530

    Main search algorithm

--- index.js
+++ index.js
@@ -1,56 +1,48 @@
 // @ts-check
 
-const corpus = ['cat', 'dog', 'cow', 'green', 'eggs', 'spam', 'mac and cheese'];
-const input = 'eg';
-
 /**
- *
  * @param { string } str
  * @returns { string }
  */
-const sortString = str => Array.from(str.toLowerCase()).sort().join('').trim();
+function sortString(str) {
+    return Array.from(str.toLowerCase()).sort().join('').trim();
+}
 
 /**
- * helper type to organise individual corpus strings and their sorted versions
- * @typedef { { original: string; sorted: string; rank?:number } } SortedCorpusItem
- */
-
-/**
- *
  * @param { string } str
+ * @param { string[] } corpus
+ * @param { { useless: boolean } } options
  * @returns { str[] }
  */
-const search = str => {
-    // empty string, so eturn eerything
+function search(str, corpus, options) {
+    // empty string, so return everything
     if (str.length === 0) {
         return corpus;
     }
 
-    /** @type { SortedCorpusItem[] } */
-    const sortedCorpusItems = corpus.map(corpusItem => ({ original: corpusItem, sorted: sortString(corpusItem) }));
-
-    const sortedInput = sortString(str);
+    const sortedCorpusItems = corpus.map(corpusItem => (options.useless ? sortString(corpusItem) : corpusItem));
+    const normalisedInput = options.useless ? sortString(str) : str;
 
     const finalResults = [];
-
     outer: for (const corpusItem of sortedCorpusItems) {
-        let currIndex = corpusItem.sorted.indexOf(sortedInput[0]);
+        let currIndex = corpusItem.indexOf(normalisedInput[0]);
 
         if (currIndex === -1) {
             continue;
         }
 
-        for (let i = 1; i < sortedInput.length; i++) {
-            let nextIndex = corpusItem.sorted.indexOf(sortedInput[i]);
-            if (nextIndex <= currIndex) {
+        for (let i = 1; i < normalisedInput.length; i++) {
+            let nextIndex = corpusItem.slice(currIndex + 1).indexOf(normalisedInput[i]);
+
+            if (nextIndex === -1) {
                 continue outer;
             } else {
-                currIndex = nextIndex;
+                currIndex += nextIndex + 1;
             }
         }
 
         finalResults.push(corpusItem);
     }
 
-    return finalResults.sort((resultA, resultB) => resultA.rank - resultB.rank).map(result => result.original);
-};
+    return finalResults;
+}
